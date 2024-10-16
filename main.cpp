91866645a6f34a83353fc3e058a8d332021ea8d6
raufkhalilov <rauf.khalilov.2004@mail.ru>
2024-10-16 14:30:15 +0300

    added post request

--- main.cpp
+++ main.cpp
@@ -2,27 +2,10 @@
 #include <pqxx/pqxx>
 #include <nlohmann/json.hpp>  
 #include <iostream>
+
 using json = nlohmann::json;
-// json get_dataClients(pqxx::connection& C) {
-//     try{
-//         pqxx::work W(C);
-//         pqxx::result R = W.exec("SELECT * FROM clients");
-//         json j;
-//         for (const auto &row : R) {
-//             j.push_back({
-//                 {"client_id", row["client_id"].is_null() ? nullptr : json(row["client_id"].as<int>())},
-//                 {"client_name", row["client_name"].is_null() ? nullptr : json(row["client_name"].as<std::string>())},
-//                 {"client_name", row["client_name"].is_null() ? nullptr : json(row["client_name"].as<std::string>())},
-//             }); 
-//         }
-//         W.commit();
-//         return j;
-//     } 
-//     catch (const std::exception &e) {
-//         std::cerr << "Error: " << e.what() << std::endl;
-//         return json{};
-//     }
-// }
+
+//---GET
 json get_dataClients(pqxx::connection& C) {
     try {
         pqxx::work W(C);
@@ -31,8 +14,8 @@ json get_dataClients(pqxx::connection& C) {
 
         for (const auto &row : R) {
             j.push_back(json{
-                {"client_id", row["client_id"].is_null() ? nullptr : json(row["client_id"].as<int>())},
-                {"client_name", row["client_name"].is_null() ? nullptr : json(row["client_name"].as<std::string>())},
+                {"client_id", row["id"].is_null() ? nullptr : json(row["id"].as<int>())}, // Изменение здесь
+                {"client_name", row["full_name"].is_null() ? nullptr : json(row["full_name"].as<std::string>())}, // Изменение здесь
                 {"phone_number", row["phone_number"].is_null() ? nullptr : json(row["phone_number"].as<std::string>())},
             });
         }
@@ -45,7 +28,6 @@ json get_dataClients(pqxx::connection& C) {
     }
 }
 
-//Функция для получения данных из базы данных
 json get_dataContracts(pqxx::connection& C) {
     try {
         pqxx::work W(C);
@@ -73,7 +55,6 @@ json get_dataContracts(pqxx::connection& C) {
     }
 }
 
-
 // Обработчик для маршрута получения данных
 crow::response handle_get_data(pqxx::connection& C, int k) {
     if(k==1)
@@ -82,11 +63,33 @@ crow::response handle_get_data(pqxx::connection& C, int k) {
         return crow::response(get_dataClients(C).dump());
 }
 
+//--POST
+crow::response add_client(pqxx::connection& C, const json& client_data) {
+    try {
+        pqxx::work W(C);
+        
+        // данные из json
+        std::string name = client_data["client_name"];
+        std::string phone = client_data["phone_number"];
+
+        W.exec_prepared("insert_clients", name, phone);
+        W.commit();
+
+        return crow::response(201);
+    } 
+    catch (const std::exception &e) {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
+        return crow::response(500);  // Код 500 - Internal Server Error
+    }
+}
 
 int main() {
     crow::SimpleApp app;  // Создаем экземпляр приложения
     pqxx::connection C("host=192.168.80.1 port=5432 dbname=arhiv user=postgres password=1");
-    
+
+    // Подготовим запрос для вставки данных
+    C.prepare("insert_clients", "INSERT INTO clients (client_name, phone_number) VALUES ($1, $2)"); // Исправлено на full_name
+
     // Route для корневого URL
     CROW_ROUTE(app, "/")([](){
         return "Hello, World!";
@@ -97,8 +100,15 @@ int main() {
         return handle_get_data(C, 1);
     });
 
+    // Route для получения всех данных таблицы clients
     CROW_ROUTE(app, "/DataClients")([&C]() {
         return handle_get_data(C, 2);
+    });
+
+    // Route для добавления клиента
+    CROW_ROUTE(app, "/addClient").methods(crow::HTTPMethod::POST)([&C](const crow::request& req) {
+        auto client_data = json::parse(req.body);  // Парсится тело запроса
+        return add_client(C, client_data);         // Добавление в бд
     });
 
     app.port(8080).multithreaded().run();    
